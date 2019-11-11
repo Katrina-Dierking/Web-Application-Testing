31ca7b66ee0e28031fe48ff24e99cea46de8cd85
Katrina_Dierking <kalynndier@gmail.com>
2019-11-11 18:24:31 -0500

    starting over AGAIN. Dashboard created.

---
+++ scoreboard/components/dashboard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Dashboard = ({ball, strike, foul, hit}) => {
+
+    return (
+        <section className = "dashboard">
+            <button onClick = {ball}>Add a Ball</button>
+            <button onClick = {strike}>Add a Strike</button>
+            <button onClick = {foul}>Add a Foul</button>
+            <button onClick = {hit}>Add a Hit</button>
+        </section>
+    );
+};
+
+export default Dashboard;
+        
+    
+
+    
+export default Dashboard;
+
+// - balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+// - balls and strikes reset to 0 when a `hit` is recorded.
+// - a `foul` increases strikes up to 2. With no strikes, 
+// a foul makes it 1 strike. 
+// With 1 strike, a foul makes it 2 strikes.
+//  With two strikes a foul has no effect, count stays at 2 strikes.
+
+// - provide a button that the person in charge
+//  can press every time there is a `strike`, `ball`, `foul` or `hit`.
+// - there is **NO** need to specify the type of hit (single, double, etc).
---
+++ scoreboard/components/display.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Display extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render () {
+        return (
+            <div>
+
+            </div>
+
+        );
+    }
+}
+    
+
+export default Display;
+
+// ### Display
+
+// - display the count of `balls` and `strikes` for the at-bat.
+// - should be updated when the user records activity on the `Dashboard` component.
